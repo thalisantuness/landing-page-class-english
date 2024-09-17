8d58081cdc102a189076adb32d2fb4a97b2928d7
Thalis Antunes <thalisantunessilva@gmail.com>
2024-09-17 16:54:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./sections/NavBar";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="container">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sections/NavBar/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function NavBar() {
+  return (
+    <header className="navbar">
+    <h1>Olá mundo</h1>
+    </header>
+  );
+}
